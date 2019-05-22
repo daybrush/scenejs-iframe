a58f6499127eef0e4d98d521991273056cb96b2e
Daybrush <daybrush@gmail.com>
2019-05-23 01:26:06 +0900

    chore: Release 0.0.1

---
+++ dist/iframe.cjs.js
@@ -0,0 +1,146 @@
+/*
+Copyright (c) 2019 Daybrush
+name: @scenejs/iframe
+license: MIT
+author: Daybrush
+repository: git+https://github.com/daybrush/scenejs-iframe.git
+version: 0.0.1
+*/
+'use strict';
+
+var scenejs = require('scenejs');
+var utils = require('@daybrush/utils');
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+/* global Reflect, Promise */
+var extendStatics = function (d, b) {
+  extendStatics = Object.setPrototypeOf || {
+    __proto__: []
+  } instanceof Array && function (d, b) {
+    d.__proto__ = b;
+  } || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+  };
+
+  return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+  extendStatics(d, b);
+
+  function __() {
+    this.constructor = d;
+  }
+
+  d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+var IframeItem =
+/*#__PURE__*/
+function (_super) {
+  __extends(IframeItem, _super);
+
+  function IframeItem() {
+    return _super !== null && _super.apply(this, arguments) || this;
+  }
+
+  var __proto = IframeItem.prototype;
+
+  __proto.setTime = function (time, isTick, isParent, parentEasing) {
+    _super.prototype.setTime.call(this, time, isTick, isParent, parentEasing);
+
+    var iterationTime = this.getIterationTime();
+    this.sendMessage((isTick ? "animate" : "time") + ":" + iterationTime);
+    return this;
+  };
+
+  __proto.end = function () {
+    _super.prototype.end.call(this);
+
+    this.sendMessage("end");
+    return this;
+  };
+
+  __proto.pause = function () {
+    _super.prototype.pause.call(this);
+
+    this.sendMessage("pause");
+    return this;
+  };
+
+  __proto.start = function (delay) {
+    var result = _super.prototype.start.call(this, delay);
+
+    this.sendMessage("start");
+    return result;
+  };
+
+  __proto.sendMessage = function (message) {
+    var elements = this.getElements();
+    elements.forEach(function (el) {
+      if (!el.contentWindow) {
+        return;
+      }
+
+      el.contentWindow.postMessage("scene:" + message, "*");
+    });
+  };
+
+  return IframeItem;
+}(scenejs.SceneItem);
+
+function register(item) {
+  utils.addEvent(window, "message", function (e) {
+    var data = e.data;
+
+    if (!utils.isString(data)) {
+      return;
+    }
+
+    var result = /(?<=^scene\:)([^:]+)(?:\:([^:]+))?/g.exec(data);
+
+    if (!result) {
+      return;
+    }
+
+    var type = result[1];
+    var value = result[2];
+
+    switch (type) {
+      case "start":
+        item.start(0);
+        break;
+
+      case "pause":
+        item.pause();
+        break;
+
+      case "end":
+        item.end();
+        break;
+
+      case "time":
+      case "animate":
+        item.setTime(parseFloat(value), type === "animate", true);
+        break;
+    }
+  });
+}
+
+exports.default = IframeItem;
+exports.register = register;
+//# sourceMappingURL=iframe.cjs.js.map
